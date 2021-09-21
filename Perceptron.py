8acbe409f3e5fa328048125265f562ed2707c8e8
benocarlson <benocarlson@gmail.com>
2021-09-21 13:57:52 -0600

    Get Debug working correctly

--- Perceptron.py
+++ Perceptron.py
@@ -1,21 +1,26 @@
 import numpy as np
 from utils import *
 
+BIAS = 1
+
 
 class Perceptron:
 
-    def __init__(self, num_inputs, learning_rate=0.1, min_change=0.0):
+    def __init__(self, num_inputs, init_weights=0):
         self.num_inputs = num_inputs
-        self.weights = np.zeros([self.num_inputs], dtype=float)
-        self.learning_rate = learning_rate
-        self.min_change = min_change
+        self.weights = np.zeros([self.num_inputs + 1], dtype=float)  # Include bias weight
+        self.init_weights = init_weights
+        self.weights[:] = init_weights
 
     def fire(self, features):
-        if len(features) != len(self.weights):
+        if len(features) + 1 != len(self.weights):
             return
         total = 0.0
         for i in range(len(self.weights)):
-            total += features[i] * self.weights[i]
+            if i == len(features):
+                total += BIAS * self.weights[i]
+            else:
+                total += features[i] * self.weights[i]
         if total > 0.0:
             return 1
         else:
@@ -30,25 +35,33 @@ class Perceptron:
             predictions[i] = self.fire(inputs[i])
         return predictions
 
-    def fit(self, inputs, targets):
+    def fit(self, inputs, targets, learning_rate, do_shuffle=False, deterministic=False, epochs=10):
         # expect inputs to be an array of arrays of features
         # expect targets to be an array of targets, one per array of features
         best_weights = self.get_weights()
         count = 0
         old_score = 0.0
-        while count < 10:
+        while count < epochs:
             count += 1
-            shuffled = shuffle(inputs)
-            for i in range(len(shuffled)):
-                output = self.fire(shuffled[i])
+            if do_shuffle:
+                epoch = shuffle(inputs)
+            else:
+                epoch = inputs
+            for i in range(len(epoch)):
+                output = self.fire(epoch[i])
                 if output != targets[i]:
                     for j in range(len(self.weights)):
-                        weight_change = self.learning_rate * (targets[i] - output) * shuffled[i][j]
-                        self.weights[i] += weight_change
+                        if j == len(self.weights) - 1:
+                            weight_change = learning_rate * (targets[i] - output) * BIAS
+                        else:
+                            weight_change = learning_rate * (targets[i] - output) * epoch[i][j]
+                        self.weights[j] += weight_change
             score = self.score(inputs, targets)
             if old_score < score:
-                count = 0
+                if not deterministic:
+                    count = 0
                 old_score = score
+            if score >= old_score:
                 best_weights = self.get_weights()
         self.weights = best_weights
         return old_score
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from Perceptron import *
 from arff import Arff
 
+mat = Arff("./data_banknote_authentication.arff")
+data = mat.data[:, 0:-1]
+labels = mat.data[:, -1].reshape(-1, 1)
+perceptron = Perceptron(mat.features_count, init_weights=0)
 
 
-
+perceptron.fit(data, labels, learning_rate=0.1, do_shuffle=False, deterministic=True, epochs=10)
+score = perceptron.score(data, labels, )
+print("Accuracy = [{:.2f}]".format(score))
+print("Final Weights =", perceptron.get_weights())
 
 
